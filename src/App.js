930c73e2a3befc65311bec8832a45e7dd1314f35
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-01 09:46:46 +0100

    removed unnacesasary files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Weather from"./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
